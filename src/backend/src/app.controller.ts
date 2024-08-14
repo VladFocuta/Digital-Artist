a4e879598f8f3abc6876e86db6e387b1b16274db
Vlad Focuta <132595962+VladFocuta@users.noreply.github.com>
2024-08-14 21:17:30 +0200

    Create app.controller.ts

---
+++ src/backend/src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { AppService } from './app.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  @UseInterceptors(FilesInterceptor('image'))
+  uploadFile(@UploadedFiles() file) {
+    console.log(file)
+  }
+}
