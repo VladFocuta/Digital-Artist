890b53d8b81429843c83fef9a01531d8309f09a6
Vlad Focuta <132595962+VladFocuta@users.noreply.github.com>
2024-08-14 21:23:52 +0200

    Create files-upload.controller.ts

---
+++ src/backend/src/files-upload.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+@Controller('files-upload')
+export class FilesUploadController {
+    @Post('upload/:userId')
+    @UseInterceptors(FileInterceptor('image', {
+        storage: diskStorage({
+            destination: './uploads',
+            filename: (req, file, callback) => {
+                const uniqueSuffix = Date.now() + '-' + req.body.userId + extname(file.originalname) + '.png';
+                callback(null, uniqueSuffix);
+            },
+        }),
+    }))
+    uploadImage(@UploadedFile() file: Express.Multer.File) {
+        console.log(file);
+        return { message: 'File uploaded successfully!' };
+    }
+
+    @Post('upload/:userId')
+    @UseInterceptors(FileInterceptor('image', {
+        storage: diskStorage({
+            destination: './dist/uploads',
+            filename: (req, file, callback) => {
+                const uniqueSuffix = Date.now() + '-' + req.body.userId + extname(file.originalname) + '.png';
+                callback(null, uniqueSuffix);
+            },
+        }),
+    }))
+    uploadImages(@UploadedFile() file: Express.Multer.File) {
+        console.log(file);
+        return { message: 'File uploaded successfully!' };
+    }
+}
